a23fe81d91de2868d72cf10948dac357a16a1b0c
NuclearBlox <nukecorebf@gmail.com>
2026-08-08 01:49:43 -0600

    Refitting cache logic for local list logic

--- Utils/localList.js
+++ Utils/localList.js
@@ -1,19 +1,16 @@
 // File for handling the users personal blacklist and whitelist. for V6
-
+// allowed is true or false. true is whitelist, false is blacklist.
 
 
 async function consultLocal(key) { 
     try {
         const stored = await chrome.storage.local.get(key);
         const local = stored[key];
-        if (cached && (Date.now() < cached.expiresAt)) {
-            return cached.data;
-        } else if (cached && Date.now() > cached.expiresAt) {
-            console.log(`[SoundProof] Cache expired for ${key}. Fetching fresh data...`,);
-            await chrome.storage.local.remove(key);
+        if (local) {
+            return local.data;
             return false;
         }
-        console.log(`[SoundProof] Cache miss for ${key}. Fetching fresh data...`,);
+        console.log(`[SoundProof] Local miss for ${key}. Fetching fresh data...`,);
         return false;
 
     } catch (err) {
@@ -22,30 +19,25 @@ async function consultLocal(key) {
     }
 } 
 
-async function updateLocal(key, statusdata) {
+async function updateLocal(key, allowStatus) {
     await chrome.storage.local.set({
-        
-        [key]: { 
-            data: statusdata, 
-            expiresAt: Date.now() + getTimeValid(statusdata)
-        } 
+        [key]: allowStatus, 
     
     });
-    console.log(`[SoundProof] Cache updated for ${key}:`, statusdata);
 
+    console.log(`[SoundProof] Local updated for ${key}:`, allowStatus);
+}
+async function removeLocal(key, allowStatus) {
+    await chrome.storage.local.remove(key);
+    console.log(`[SoundProof] Local updated for ${key}:`, allowStatus);
 }
 
 
-async function LocalStatus(artist, platformClass, forcedRefresh) { // checks the cache first, if not found or expired, fetches from Supabase and updates the cache
-    const key = `sp_${platformClass}_${artist.toLowerCase().trim()}`;
+async function LocalStatus(artist, platformClass) { 
+    const key = `sp_local_${platformClass}_${artist.toLowerCase().trim()}`;
 
-    if (forcedRefresh == false) {
-        const cachedData = await consultCache(key);
-        console.log(`[SoundProof] Cache check for ${artist} on ${platformClass}:`, cachedData ? 'HIT' : 'MISS');
-        if (cachedData) return  cachedData;
-    }
-    console.log(`[SoundProof] Forced refresh for ${artist} on ${platformClass}...`);
-    const liveData = await fetchDataFromSupabase(artist, platformClass);
-    await updateCache(key, liveData);
-    return liveData;
+        const localData = await consultLocal(key);
+        console.log(`[SoundProof] Cache check for ${artist} on ${platformClass}:`, localData ? 'HIT' : 'MISS');
+        if (localData) return  localData;
+    
 }
